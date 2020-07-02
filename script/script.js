63440098bfcdeba5cb46891b521cbc2d0c73cb02
Luke Stewart <47406546+TheRadDesigner@users.noreply.github.com>
2020-07-02 20:56:36 +1000

    End of day 2/07/2020

---
+++ script/script.js
@@ -0,0 +1,7 @@
+// Page Naviagtion
+
+$("#goButton").on("click", function() {
+    $(".page").html("<div class="page"><div class="infoContainer"><div class="info"><div class="title">BENEFITS<br>THAT<br>OUTWEIGH<br>THE COSTS</div><h2>We get it, you’re not going to outlay these costs unless there’s some benefit. We got you covered look over here for some benefits of hiring an Intern</h2></div></div><div class="formContainer"><div class="form" id="basicsForm"><div class="formHeader"><h1>First Lets Get Some Basic Details</h1></div><br>");
+      <div class="sliderGroup"><h3>What is your Intern’s hourly pay rate?</h3><br><div class="sliderContainer"><div class="sliderInnerContainer"><div class="sliderValue"><div class="sliderMin"><h3>$0</h3></div><div class="sliderMax">
+);
+  });
